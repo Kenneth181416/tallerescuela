43470552e566b3133027ae2ffb2c26aa22f8d7aa
kenneth ibarra <kennethivelasco1118@gmail.com>
2025-05-27 11:31:02 -0500

    Se inicia el proyecto

---
+++ Tallerescuela1/src/main/java/com/mycompany/tallerescuela1/Tallerescuela1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tallerescuela1;
+
+/**
+ *
+ * @author Andres Ibarra
+ */
+public class Tallerescuela1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
